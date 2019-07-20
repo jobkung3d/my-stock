5e978f21b08c1081344ff6348ce84c19a6d6e2b6
jobkung3d <crystan_job.a@hotmail.com>
2019-07-21 02:08:31 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const books = require('./db')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/books', (req,res)=>{
+    res.json(books) 
 })
 
 app.listen(3000, ()=>{
