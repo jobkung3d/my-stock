df540981ef1fbc93150d36053259ab50f1d90517
jobkung3d <crystan_job.a@hotmail.com>
2019-08-10 13:39:59 +0700

    update

--- client/src/page/products/list.js
+++ client/src/page/products/list.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layouts from '../../component/layout';
+import TableList from '../../component/table';
 
 class ProductList extends Component{ 
     render(){
         return(
             <div>
                 <Layouts breadCrumb={["Products","List"]} menuKey="2">
-                    Products List
+                    <TableList />
                 </Layouts>
             </div>
         )
